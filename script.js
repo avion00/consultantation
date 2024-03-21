4224b5a4a685609bbf368e063df893338b352b61
Abhishek kumar chaudhary <avioncompany00@gmail.com>
2024-03-22 02:22:02 +0545

    try to check it out

--- script.js
+++ script.js
@@ -1,15 +1,56 @@
-// navbar hover effect
 const listItemsWithOptions = document.querySelectorAll(".list_item");
 
 listItemsWithOptions.forEach((item) => {
   const optionsList = item.querySelector(".options");
+  const arrowIcon = item.querySelector(".icon svg");
 
-  // Add event listeners for hover effect
   item.addEventListener("mouseenter", () => {
-    optionsList.style.display = "block"; // Display options on hover
+    optionsList.style.display = "block"; 
+    arrowIcon.style.transform = "rotate(180deg)"; 
   });
 
   item.addEventListener("mouseleave", () => {
-    optionsList.style.display = "none"; // Hide options when not hovered
+    optionsList.style.display = "none"; 
+    arrowIcon.style.transform = "rotate(0deg)"; 
+  });
+});
+
+
+
+// for drop down 
+
+const optionMenus = document.querySelectorAll(".select-menu");
+
+optionMenus.forEach((optionMenu) => {
+  const selectBtn = optionMenu.querySelector(".select-btn");
+  const sBtn_text = selectBtn.querySelector(".sBtn-text");
+  const options = optionMenu.querySelectorAll(".options .option");
+
+  selectBtn.addEventListener("click", (event) => {
+    event.stopPropagation(); // Prevents the click event from bubbling up
+    optionMenu.classList.toggle("active");
+    console.log("Button clicked");
+  });
+
+  options.forEach((option) => {
+    option.addEventListener("click", (event) => {
+      event.preventDefault(); // Prevents the default action of the anchor tag
+      let selectedOption = option.querySelector(".option-text").innerText;
+      sBtn_text.innerText = selectedOption;
+      console.log(selectedOption);
+      optionMenu.classList.remove("active");
+      console.log("Option clicked");
+    });
+  });
+
+  // Add event listener to the document body
+  document.body.addEventListener("click", (event) => {
+    // Check if the clicked element is not part of the select menu
+    if (
+      !optionMenu.contains(event.target) &&
+      !selectBtn.contains(event.target)
+    ) {
+      optionMenu.classList.remove("active"); // Hide the select menu
+    }
   });
 });
